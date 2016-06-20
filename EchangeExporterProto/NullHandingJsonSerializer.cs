18d08832f04768163d91d47d479e3fbbdf66188f
fabien <fvignon@linagora.com>
2016-06-20 15:30:50 +0200

    Initial commit with contributors
    read all Contacts in default addressbook, and dump them into a queue.

---
+++ EchangeExporterProto/NullHandingJsonSerializer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+using EasyNetQ;
+using Newtonsoft.Json;
+
+namespace EchangeExporterProto {
+    class NullHandingJsonSerializer : ISerializer
+    {
+        private readonly ITypeNameSerializer typeNameSerializer;
+
+        private readonly JsonSerializerSettings serializerSettings = new JsonSerializerSettings
+        {
+            TypeNameHandling = TypeNameHandling.Auto,
+            NullValueHandling = NullValueHandling.Ignore,
+            Error = (serializer,err) => err.ErrorContext.Handled = true,
+        };
+
+        public NullHandingJsonSerializer(ITypeNameSerializer typeNameSerializer)
+        {
+            if (typeNameSerializer == null)
+                throw new ArgumentNullException("typeNameSerializer");
+            this.typeNameSerializer = typeNameSerializer;
+        }
+
+        public byte[] MessageToBytes<T>(T message) where T : class
+        {
+            if (message == null)
+                throw new ArgumentNullException("message");
+            return Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message, serializerSettings));
+        }
+
+        public T BytesToMessage<T>(byte[] bytes)
+        {
+            if (bytes == null)
+                throw new ArgumentNullException("bytes");
+            return JsonConvert.DeserializeObject<T>(Encoding.UTF8.GetString(bytes), serializerSettings);
+        }
+
+        public object BytesToMessage(string typeName, byte[] bytes)
+        {
+            if (typeName == null)
+                throw new ArgumentNullException("typeName");
+            if (bytes == null)
+                throw new ArgumentNullException("bytes");
+            var type = typeNameSerializer.DeSerialize(typeName);
+            return JsonConvert.DeserializeObject(Encoding.UTF8.GetString(bytes), type, serializerSettings);
+        }
+    }}
---
+++ EchangeExporterProto/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net;
+using System.Linq;
+using EasyNetQ;
+using Messages;
+using Microsoft.Exchange.WebServices.Data;
+
+namespace EchangeExporterProto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExchangeService service = new ExchangeService(ExchangeVersion.Exchange2010_SP2);
+            ServicePointManager.ServerCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true;
+            service.Credentials = new WebCredentials("user1@MSLABLGS", "L1n4g0r4");
+            service.Url = new Uri("https://172.16.24.101/EWS/Exchange.asmx");
+
+            var fetchView = new ItemView(int.MaxValue);
+            fetchView.PropertySet = new PropertySet(BasePropertySet.FirstClassProperties);
+
+            //Newtonsoft.Json.JsonSerializer _jsonWriter = new Newtonsoft.Json.JsonSerializer {
+            //    NullValueHandling = NullValueHandling.Ignore
+            //};
+
+            var queueConnectionString = "host=10.69.0.117";
+
+            // using (var bus = RabbitHutch.CreateBus(queueConnectionString, RegisterSerializer(_jsonWriter)))
+            using (var bus = RabbitHutch.CreateBus(queueConnectionString, serviceRegister => serviceRegister.Register<ISerializer>(
+                    serviceProvider => new NullHandingJsonSerializer(new TypeNameSerializer()))))             
+            {
+                service.FindItems(WellKnownFolderName.Contacts, fetchView)
+                    .Select(c => Contact.Bind(service, c.Id))
+                    // .Cast<Contact>()
+                    .ToList()
+                    .ForEach(contact => bus.Publish(contact));
+            }
+            Console.ReadLine();
+            // PublishMessage();
+        }
+
+        //private static Action<IServiceRegister> RegisterSerializer(Newtonsoft.Json.JsonSerializer _jsonWriter) {
+        //    return serviceRegister => serviceRegister.Register<ISerializer>(serviceProvider => _jsonWriter);
+        //}
+
+        private static void PublishMessage() {
+            using (var bus = RabbitHutch.CreateBus("host=10.69.0.117")) {
+                var input = "";
+                Console.WriteLine("Enter a message. 'Quit' to quit.");
+                while ((input = Console.ReadLine()) != "Quit")
+                    bus.Publish(new TextMessage {
+                        Text = input
+                    });
+            }
+        }
+    }
+}
