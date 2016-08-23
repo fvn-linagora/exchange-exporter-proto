5b3e750a32ce1eed232d98bebcf47b1af442e3e4
Fabien Vignon <fvignon@linagora.com>
2016-08-23 10:55:04 +0200

    add project ServiceBus.Interface

---
+++ src/ServiceBus.Interfaces/IServiceBus.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reactive.Concurrency;
+using System.Threading.Tasks;
+
+namespace ServiceBus.Interfaces
+{
+    public interface IMessageEmitter
+    {
+        Task Send(ICommand command);
+
+        Task Publish(IEvent ev);
+    }
+
+    public interface IServiceBusClient
+    {
+        IDisposable Subscribe(object subscriber, IScheduler scheduler = null);
+    }
+
+    public interface IServiceBus : IServiceBusClient, IMessageEmitter
+    {
+    }
+
+    public interface IMessage
+    {
+    }
+
+    public interface ICommand
+    {
+    }
+    public interface IEvent
+    {
+    }
+}
